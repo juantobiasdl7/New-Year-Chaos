0dfc56d60d41f00b9631f5738a82d57fe09f5092
Tobías <juantobiasdl7@gmail.com>
2022-03-26 23:57:51 +0000

    Too chaotic validation

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 let queue = [2,1,5,3,4];
-
+let queue2 = [1,2,5,3,7,8,6,4];
+let queue3 = [5,1,2,3,7,8,6,4];
+//1 2 5 3 7 8 6 4
 //1 2 5 3 7 8 6 4
 
 
 function minimumBribes(q) {
     // Write your code here
   let bribe = 0;
-  let chaoticCounter = 0;
-  let chaoticDecider = false;
-  
-  for (let i = 0; i < q.length-1; i++) {
-
-    let counter = i;
-    chaoticCounter = 0;
-    console.log("Numero de iteración: " + i);      
-      
-    while (q[i] > q[counter+1] && q[counter+1] != q[i]-1) {
-      console.log(i);      
-      console.log(counter);
-      counter ++;
-      bribe ++;
-      chaoticCounter ++;
-      
-      if(chaoticCounter > 2){
-        chaoticDecider = true;
-      }
-      
+   
+  for (let i = q.length-1; i >=0; i--) {
+    //console.log("Iteration: " + i);
+    //console.log(q[i] + " - " + (i+1) + " = " + (q[i]-(i+1)));
+    if( q[i] - (i+1) > 2){
+      //console.log(q[i] + " " + (i+1));
+      console.log("Too chaotic");
     }
-  }
+    
+  } 
 
-  if(chaoticDecider == true){
-    console.log("Too chaotic!");
-  }else{
-    console.log(bribe);
-  }
-  
 }
 
-minimumBribes(queue);
+
+minimumBribes(queue3);
